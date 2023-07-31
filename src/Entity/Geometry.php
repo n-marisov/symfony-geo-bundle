16e6dae1a7854f83d792689d4611e8720be1e31d
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-07-31 12:29:43 +0300

    79

--- src/Entity/Geometry.php
+++ src/Entity/Geometry.php
@@ -31,7 +31,7 @@ abstract class Geometry implements IteratorAggregate, Countable, ArrayAccess, Js
 
     public static function getCalculator():?GeoCalculator
     {
-        $calculator = self::$calculator?->get("geo.calculator.ellipsoidal");
+        $calculator = self::$calculator->get("geo.calculator.ellipsoidal");
         dump($calculator);
         if(is_a($calculator,GeoCalculator::class))
             return $calculator;
