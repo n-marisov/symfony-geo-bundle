89256ce6fa39e9b9c0514d98edc572cfaeecf966
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-07-31 14:50:53 +0300

    83

--- src/Factory/CalculatorFactory.php
+++ src/Factory/CalculatorFactory.php
@@ -2,11 +2,31 @@
 
 namespace Maris\Symfony\Geo\Factory;
 
+use http\Encoding\Stream;
 use Maris\Symfony\Geo\Calculator\EllipsoidalCalculator;
 use Maris\Symfony\Geo\Calculator\GeoCalculator;
 
 class CalculatorFactory
 {
+    /**
+     * @var class-string
+     */
+    protected string $calculatorClass;
+
+    /**
+     * @param string $calculatorClass
+     */
+    public function __construct( string $calculatorClass )
+    {
+        $this->calculatorClass = $calculatorClass;
+    }
+
+    public function __invoke():GeoCalculator
+    {
+        return new $this->calculatorClass();
+    }
+
+
     public static function createCalculator( ... $args ):GeoCalculator
     {
         dump($args);
