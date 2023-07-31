40c9e25adb916d5060a299c0751d1932e6878b1d
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-07-31 16:20:30 +0300

    95

--- src/EventListener/EntityUpdateListener.php
+++ src/EventListener/EntityUpdateListener.php
@@ -9,6 +9,18 @@ use Maris\Symfony\Geo\Entity\Location;
 #[AsDoctrineListener(event: 'loadClassMetadata')]
 class EntityUpdateListener
 {
+
+    protected int $precision;
+
+    /**
+     * @param int $precision
+     */
+    public function __construct( int $precision )
+    {
+        $this->precision = $precision;
+    }
+
+
     public function __invoke( LoadClassMetadataEventArgs $args ):void
     {
         $classMetaData = $args->getClassMetadata();
@@ -16,11 +28,11 @@ class EntityUpdateListener
         if($classMetaData->name !== Location::class)
             return;
 
-        $classMetaData->fieldMappings["latitude"]["precision"] = 8 + 1;
-        $classMetaData->fieldMappings["latitude"]["scale"] = 6 + 1;
+        $classMetaData->fieldMappings["latitude"]["precision"] = 2 + $this->precision;
+        $classMetaData->fieldMappings["latitude"]["scale"] = $this->precision;
 
-        $classMetaData->fieldMappings["longitude"]["precision"] = 9 + 1;
-        $classMetaData->fieldMappings["longitude"]["scale"] = 6 + 1;
+        $classMetaData->fieldMappings["longitude"]["precision"] = 3 + $this->precision;
+        $classMetaData->fieldMappings["longitude"]["scale"] = $this->precision;
     }
 
 }
