4ffd07062bf88968acca93bc9423f859ffa04381
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-07-31 15:55:27 +0300

    91

--- src/EventListener/EntityUpdateListener.php
+++ src/EventListener/EntityUpdateListener.php
@@ -4,13 +4,17 @@ namespace Maris\Symfony\Geo\EventListener;
 
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
 use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Maris\Symfony\Geo\Entity\Location;
 
 #[AsDoctrineListener(event: 'loadClassMetadata')]
 class EntityUpdateListener
 {
     public function __invoke( LoadClassMetadataEventArgs $args ):void
     {
-        dump($args);
+        $classMetaData = $args->getClassMetadata();
+
+        if($classMetaData->name === Location::class)
+            dump($classMetaData);
     }
 
 }
